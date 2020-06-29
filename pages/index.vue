5a5cdbf4c5b92ba364ead16364d25f2a10a36586
gospelsharmony <gospelsharmony@gmail.com>
2020-06-29 07:31:13 +0200

    make harmony of gospel page be the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,61 @@
 <template>
-  <div class="routes">
+  <main class="gospels-root">
     <!-- As the whole initialisation is lifecycle hook driven, it -->
     <!-- is cruical to have control over component creation order. -->
     <!-- Component creation order can be defined here. -->
     <timelineBuilder />
     <htmlOutput />
-    <nuxt-link to="/harmony-of-gospels">Harmony of Gospels</nuxt-link>
-    <nuxt-link to="/about">About</nuxt-link>
-  </div>
+    <AccordionMain 
+      v-for="(group,index) in gdata.htmlOutput"
+      :key="index"
+      v-bind="{ 
+        groupIndex: index,
+        groupTitle: (Object.keys(group)).toString()
+      }" />
+    <Map />
+    <!-- <ButtonMap /> -->
+    <!-- <ButtonMenu /> -->
+  </main>
 </template>
 
 <script>
+import { gdata } from "~/components/gdata";
 import timelineBuilder from "~/components/TimelineBuilder";
 import htmlOutput from "~/components/HtmlOutput";
+import AccordionMain from "~/components/AccordionMain";
+import Map from "~/components/Map"
+// import ButtonMap from "~/components/ButtonMap"
+// import ButtonMenu from "~/components/ButtonMenu"
 
 export default {
+
   components: {
     timelineBuilder,
-    htmlOutput
-  }
+    htmlOutput,
+    AccordionMain,
+    Map,
+    // ButtonMap,
+    // ButtonMenu
+  },
+
+  data () {
+    return {
+      gdata
+    }
+  },
+
 }
 </script>
 
 
-<style scoped>
-
+<style>
 html {
   font-size: 100%;
 }
 
-.routes {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-}
+/* .gospels-root {
+  
+} */
+
 
 </style>
